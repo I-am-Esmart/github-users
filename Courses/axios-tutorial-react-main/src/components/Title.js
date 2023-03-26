78ba1535e503bc68a9a58918f6495ac5e758e44d
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2023-03-26 20:38:20 +0100

    generate joke randomly

---
+++ Courses/axios-tutorial-react-main/src/components/Title.js
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>
+        <span>Joke</span> Generator
+      </h1>
+    </div>
+  );
+};
+export default Title;
---
+++ Courses/axios-tutorial-react-main/src/examples/1-first-request.js
@@ -0,0 +1,26 @@
+// import { useEffect } from "react";
+// import axios from "axios";
+
+// const url = "https://course-api.com/react-store-products";
+// const FirstRequest = () => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios(url);
+//       const result = response.data;
+//       console.log(result);
+//     } catch (error) {
+//       console.log(error.response);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <h2 className="text-center">
+//       first request <br />
+//     </h2>
+//   );
+// };
+// export default FirstRequest;
